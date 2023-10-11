281437fa27afaede147740a3b7d09d1b45cbf620
jkim-012 <jkim00456@gmail.com>
2023-10-11 22:11:08 +0900

    Initial commit

---
+++ src/main/java/jpabook/jpashop/domain/Order.java
@@ -0,0 +1,29 @@
+package jpabook.jpashop.domain;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "orders")
+public class Order {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "order_id")
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+    private List<OrderItems> orderItems = new ArrayList<>();
+
+    private Delivery delivery;
+
+    private LocalDateTime orderDate;
+
+
+
+}
